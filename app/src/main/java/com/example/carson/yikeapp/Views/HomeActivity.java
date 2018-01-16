e9576470e6222a9abdd92ddaaa4d16108c7ff89d
Administrator <2323170229@qq.com>
2018-01-16 09:34:39 +0800

    添加了首页activity

---
+++ app/src/main/java/com/example/carson/yikeapp/Views/HomeActivity.java
@@ -0,0 +1,145 @@
+package com.example.carson.yikeapp.Views;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.carson.yikeapp.R;
+import com.example.carson.yikeapp.Views.dummy.HomeContent;
+
+public class HomeActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private HomePagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the four
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new HomePagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListFragmentInteraction(HomeContent.BNBHomeItem item) {
+        //TODO 点击了一个recycleview 的item
+        Toast.makeText(this,"Item "+ item.id+" clicked.",Toast.LENGTH_SHORT);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home_item_list, container, false);
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class HomePagerAdapter extends FragmentPagerAdapter {
+
+        public HomePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return ItemFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 4 total pages.
+            return 4;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/carson/yikeapp/Views/dummy/HomeContent.java
@@ -0,0 +1,80 @@
+package com.example.carson.yikeapp.Views.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class HomeContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<BNBHomeItem> ITEMS = new ArrayList<BNBHomeItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, BNBHomeItem> ITEM_MAP = new HashMap<String, BNBHomeItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+        //TODO 测试创建items
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createBNBHomeItem(i));
+        }
+    }
+
+    private static void addItem(BNBHomeItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static BNBHomeItem createBNBHomeItem(int position) {
+        //TODO:返回一个新的item
+        return new BNBHomeItem(String.valueOf(position), "Item " + position, makeDetails(position),null);
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class BNBHomeItem {
+        public final String id;
+        public final String name;
+        public final String host;
+        public final String time;
+        public final String duration;
+
+        //TODO：设置传入值-id-name-host-time-duration
+        public BNBHomeItem(String id, String content, String details,String duration) {
+            this.id = id;
+            this.name = id;
+            this.host = content;
+            this.time = details;
+            this.duration = duration;
+        }
+
+        @Override
+        public String toString() {
+            //TODO: 设置tostring
+            return name+host+time+duration;
+        }
+    }
+}
